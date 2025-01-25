6d8e445f331c98d788b52077b071dcc8fcadb532
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-24 21:34:06 -0800

    Working faucet

--- Assets/Scripts/Faucet/DropletSpawning.cs
+++ Assets/Scripts/Faucet/DropletSpawning.cs
@@ -12,48 +12,54 @@ namespace Faucet
         [SerializeField] float spawnInterval = 0.2f; // Interval in seconds
         [SerializeField] float particleLifetime = 15f; // Lifetime of the particles in seconds
 
+        [SerializeField] float forceAmount = 10f; // Amount of force to apply
+        [SerializeField] float coneAngle = 30f; // Angle of the cone in degrees
 
-        private InputActions _inputActions;
-        private Coroutine _spawnCoroutine;
 
-        private void Awake() =>
+        private InputActions _InputActions;
+        private Coroutine _SpawnCoroutine;
+
+        private void Awake()
+        {
             // Initialize the InputActions
-            _inputActions = new InputActions();
+            _InputActions = new InputActions();
+        }
+
 
         private void OnEnable()
         {
             // Enable the InputActions
-            _inputActions.Enable();
+            _InputActions.Enable();
 
             // Subscribe to the button hold action
-            _inputActions.Faucet.SpewWater.started += OnSpewWaterButtonPressed;
-            _inputActions.Faucet.SpewWater.canceled += OnSpewWaterButtonReleased;
+            _InputActions.Faucet.SpewWater.started += OnSpewWaterButtonPressed;
+            _InputActions.Faucet.SpewWater.canceled += OnSpewWaterButtonReleased;
         }
 
 
         private void OnDisable()
         {
             // Disable the InputActions
-            _inputActions.Disable();
+            _InputActions.Disable();
 
             // Subscribe to the button hold action
-            _inputActions.Faucet.SpewWater.started -= OnSpewWaterButtonPressed;
-            _inputActions.Faucet.SpewWater.canceled -= OnSpewWaterButtonReleased;
+            _InputActions.Faucet.SpewWater.started -= OnSpewWaterButtonPressed;
+            _InputActions.Faucet.SpewWater.canceled -= OnSpewWaterButtonReleased;
         }
 
         private void OnSpewWaterButtonPressed(InputAction.CallbackContext obj)
         {
             // Start spawning objects
-            _spawnCoroutine = StartCoroutine(SpawnObjects());
+            _SpawnCoroutine = StartCoroutine(SpawnObjects());
         }
 
         private void OnSpewWaterButtonReleased(InputAction.CallbackContext obj)
         {
             // Stop spawning objects
-            if(_spawnCoroutine == null) return;
+            if(_SpawnCoroutine == null) return;
 
-            StopCoroutine(_spawnCoroutine);
-            _spawnCoroutine = null;
+            StopCoroutine(_SpawnCoroutine);
+            _SpawnCoroutine = null;
 
         }
 
@@ -63,6 +69,17 @@ namespace Faucet
             {
                 // Spawn the object
                 GameObject droplet = Instantiate(dropletPrefab, spawnPoint.position, spawnPoint.rotation);
+
+                float randomAngle = Random.Range(-coneAngle / 2f, coneAngle / 2f);
+                float radians = randomAngle * Mathf.Deg2Rad;
+                Vector2 randomDirection = new Vector2(Mathf.Sin(radians), -Mathf.Cos(radians)).normalized;
+
+                // Generate a random multiplier for the force amount
+                float forceVariation = Random.Range(0.2f, 1.8f); // ±20% variation
+                float adjustedForce = forceAmount * forceVariation;
+
+                Rigidbody2D rbComponent = droplet.GetComponent<Rigidbody2D>();
+                if (rbComponent) rbComponent.AddForce(randomDirection * adjustedForce, ForceMode2D.Impulse);
 
                 // Start a coroutine to destroy the particle after its lifetime
                 StartCoroutine(DestroyAfterLifetime(droplet, particleLifetime));
