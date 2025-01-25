ecf4aa37ebc02c9d2f1c5f4194f1dadcec85db3d
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-24 19:57:30 -0800

    Input stuff set up, faucet working with timer, cup colliders setup

--- Assets/Scripts/Faucet/DropletSpawning.cs
+++ Assets/Scripts/Faucet/DropletSpawning.cs
@@ -1,17 +1,85 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 namespace Faucet
 {
     public class DropletSpawning : MonoBehaviour
     {
-        [SerializeField] private GameObject spawnPoint;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private GameObject dropletPrefab;
+        [SerializeField] float spawnInterval = 0.2f; // Interval in seconds
+        [SerializeField] float particleLifetime = 15f; // Lifetime of the particles in seconds
 
-        void Start()
+
+        private InputActions _inputActions;
+        private Coroutine _spawnCoroutine;
+
+        private void Awake() =>
+            // Initialize the InputActions
+            _inputActions = new InputActions();
+
+        private void OnEnable()
         {
-            
+            // Enable the InputActions
+            _inputActions.Enable();
+
+            // Subscribe to the button hold action
+            _inputActions.Faucet.SpewWater.started += OnSpewWaterButtonPressed;
+            _inputActions.Faucet.SpewWater.canceled += OnSpewWaterButtonReleased;
         }
 
+
+        private void OnDisable()
+        {
+            // Disable the InputActions
+            _inputActions.Disable();
+
+            // Subscribe to the button hold action
+            _inputActions.Faucet.SpewWater.started -= OnSpewWaterButtonPressed;
+            _inputActions.Faucet.SpewWater.canceled -= OnSpewWaterButtonReleased;
+        }
+
+        private void OnSpewWaterButtonPressed(InputAction.CallbackContext obj)
+        {
+            // Start spawning objects
+            _spawnCoroutine = StartCoroutine(SpawnObjects());
+        }
+
+        private void OnSpewWaterButtonReleased(InputAction.CallbackContext obj)
+        {
+            // Stop spawning objects
+            if(_spawnCoroutine == null) return;
+
+            StopCoroutine(_spawnCoroutine);
+            _spawnCoroutine = null;
+
+        }
+
+        private IEnumerator SpawnObjects()
+        {
+            while (true)
+            {
+                // Spawn the object
+                GameObject droplet = Instantiate(dropletPrefab, spawnPoint.position, spawnPoint.rotation);
+
+                // Start a coroutine to destroy the particle after its lifetime
+                StartCoroutine(DestroyAfterLifetime(droplet, particleLifetime));
+
+                // Wait for the spawn interval
+                yield return new WaitForSeconds(spawnInterval);
+            }
+        }
+
+        private IEnumerator DestroyAfterLifetime(GameObject particle, float lifetime)
+        {
+            // Wait for the lifetime duration
+            yield return new WaitForSeconds(lifetime);
+
+            // Destroy the particle
+            Destroy(particle);
+        }
 
 
     }
