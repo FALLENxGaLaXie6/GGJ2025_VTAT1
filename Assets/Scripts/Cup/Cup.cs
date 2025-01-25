6b1ffb8fb15752ecdbd22f871bbe1f732295b498
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-25 10:19:25 -0800

    Cup changed to physics based movement, modified some of the cup parameters

--- Assets/Scripts/Cup/Cup.cs
+++ Assets/Scripts/Cup/Cup.cs
@@ -1,64 +1,58 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Cup : MonoBehaviour
 {
-
     [SerializeField] private float maxMoveSpeed = 25f;
     [SerializeField] private float maxDistance = 10f;
     [SerializeField] private float maxRotationSpeed = 720f;
     [SerializeField] private float maxRotationDistance = 180f;
     [SerializeField] private float maxRotationAngle = 90f;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        // Get the Rigidbody2D component
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Pulling the current cursor position
         Vector3 cursorLocation3D = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         // Pulling the current cup position
         Vector3 cupLocation3D = transform.position;
-        
+
         // Calculating the difference between the positions
         Vector3 deltaPosition = cursorLocation3D - cupLocation3D;
         deltaPosition.z = 0;
         float deltaMagnitude = deltaPosition.magnitude;
-        
+
         // Calculating the speed of the cup
         float moveSpeed = deltaMagnitude * (maxMoveSpeed / maxDistance);
-        if (moveSpeed > maxMoveSpeed)
-        {
-            moveSpeed = maxMoveSpeed;
-        }
-        
+        moveSpeed = Mathf.Min(moveSpeed, maxMoveSpeed);
+
         // Calculating the move direction
         Vector3 moveDirection = deltaPosition.normalized;
-        
+
+        // Move the cup using Rigidbody2D
+        rb.linearVelocity = moveSpeed * moveDirection;
+
         // Calculating the cursor angle
         float cursorAngleRad = Mathf.Atan2(-1 * deltaPosition.x, deltaPosition.y);
-        float cursorAngleDeg = (180f / Mathf.PI) * cursorAngleRad;
-        
+        float cursorAngleDeg = cursorAngleRad * Mathf.Rad2Deg;
+
         // Pulling the cup angle
         float cupAngleDeg = transform.eulerAngles.z;
-        
+
         // Normalizing rotation angles
-        if (cursorAngleDeg < 0)
-        {
-            cursorAngleDeg += 360;
-        }
-        if (cupAngleDeg < 0)
-        {
-            cupAngleDeg += 360;
-        }
+        if (cursorAngleDeg < 0) cursorAngleDeg += 360;
+        if (cupAngleDeg < 0) cupAngleDeg += 360;
 
         // Calculating the distance between the cursor and the cup in each direction
-        float distanceCCW;
-        float distanceCW;
+        float distanceCCW, distanceCW;
         if (cupAngleDeg > cursorAngleDeg)
         {
             distanceCW = cupAngleDeg - cursorAngleDeg;
@@ -81,37 +75,16 @@ public class Cup : MonoBehaviour
             rotationSpeed = -1 * (distanceCW / maxRotationDistance) * maxRotationSpeed;
         }
 
-        if (rotationSpeed > maxRotationSpeed)
-        {
-            rotationSpeed = maxRotationSpeed;
-        }
-        else if (rotationSpeed < -maxRotationSpeed)
-        {
-            rotationSpeed = -maxRotationSpeed;
-        }
-        
+        rotationSpeed = Mathf.Clamp(rotationSpeed, -maxRotationSpeed, maxRotationSpeed);
+
         // Calculating the new rotation angle
-        cupAngleDeg += rotationSpeed * Time.deltaTime;
-        if (cupAngleDeg > 180)
-        {
-            cupAngleDeg -= 360;
-        }
-        else if (cupAngleDeg < -180)
-        {
-            cupAngleDeg += 360;
-        }
+        cupAngleDeg += rotationSpeed * Time.fixedDeltaTime;
+        if (cupAngleDeg > 180) cupAngleDeg -= 360;
+        else if (cupAngleDeg < -180) cupAngleDeg += 360;
 
-        if (cupAngleDeg > maxRotationAngle)
-        {
-            cupAngleDeg = maxRotationAngle;
-        }
-        else if (cupAngleDeg < -maxRotationAngle)
-        {
-            cupAngleDeg = -maxRotationAngle;
-        }
+        cupAngleDeg = Mathf.Clamp(cupAngleDeg, -maxRotationAngle, maxRotationAngle);
 
-        // Moving and rotating the cup
-        transform.position += moveSpeed * Time.deltaTime * moveDirection;
-        transform.eulerAngles = new Vector3(0, 0, cupAngleDeg);
+        // Rotate the cup using Rigidbody2D
+        rb.MoveRotation(cupAngleDeg);
     }
 }
