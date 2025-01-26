8524c82c60d06adf2ab93e455d4e38b34967f5a8
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-25 23:40:18 -0800

    Particle tracking bar logic and some placeholder art for it

---
+++ Assets/Scripts/UI/TrackingBarUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class TrackingBarUI : MonoBehaviour
+    {
+        [SerializeField] private Image frontBarImage;
+        [SerializeField] private Image backBarImage;
+
+        private float _lerpTimer;
+        private float _chipSpeed = 2f;
+
+        private int _currentParticles = 0;
+        private int _maxParticles = 100;
+
+        private void Start()
+        {
+            _currentParticles = 0;
+        }
+
+    }
+}
