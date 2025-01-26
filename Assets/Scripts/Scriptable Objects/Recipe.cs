53b7e970e80bbf2b4e7aed6f9e4353104076bedd
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-26 10:09:25 -0800

    Recipe SO added, SFX for vacuum added

---
+++ Assets/Scripts/Scriptable Objects/Recipe.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Scriptable_Objects
+{
+    [CreateAssetMenu(fileName = "Drink Recipe", menuName = "Recipes/Drink Recipe", order = 0)]
+    public class Recipe : ScriptableObject
+    {
+        
+    }
+}
