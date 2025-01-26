0481af18a8bf3191b432a11b48356beca22a5b17
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-26 12:27:03 -0800

    Prefabbed game initialization object

---
+++ Assets/Scripts/GameRuntime/Game Flow/GameInitialization.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Scriptable_Objects;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace GameRuntime.Game_Flow
+{
+    public class GameInitialization : MonoBehaviour
+    {
+        [ListDrawerSettings(ShowFoldout = true, DraggableItems = true, ShowIndexLabels = true)] [SerializeField]
+        private List<Recipe> possibleInitialRecipes;
+
+        private Recipe _initialRecipe;
+
+        private void Start()
+        {
+            _initialRecipe = possibleInitialRecipes[Random.Range(0, possibleInitialRecipes.Count)];
+        }
+    }
+}
