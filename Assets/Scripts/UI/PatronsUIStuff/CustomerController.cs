6d2e0f69dbfb7dac25af2dbc7207ffcf58bc8877
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-26 13:26:15 -0800

    Set up customer ui stuff

---
+++ Assets/Scripts/UI/PatronsUIStuff/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace UI.PatronsUIStuff
+{
+    public class CustomerController : MonoBehaviour
+    {
+        private static readonly int EnterBarIndex = Animator.StringToHash("enterBar");
+        private static readonly int LeaveBarIndex = Animator.StringToHash("leaveBar");
+        [SerializeField] private Animator animator;
+
+
+        public void EnterBar()
+        {
+            animator.SetTrigger(EnterBarIndex);
+        }
+
+        public void ExitBar()
+        {
+            animator.SetTrigger(LeaveBarIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PatronsUIStuff/WorldSpaceCameraModifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace UI.PatronsUIStuff
+{
+    public class WorldSpaceCameraModifier : MonoBehaviour
+    {
+        private Canvas _canvas;
+        private void Awake()
+        {
+            _canvas = GetComponent<Canvas>();
+
+        }
+
+        private void Start()
+        {
+            _canvas.worldCamera = Camera.main;
+        }
+    }
+}
