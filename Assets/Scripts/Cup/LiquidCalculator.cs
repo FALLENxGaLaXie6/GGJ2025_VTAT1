0d1ae71b7331c503a9e02dcaf0fcbee52ebde149
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-25 19:38:33 -0800

    Prefabbed my working part of the cup

--- Assets/Scripts/Cup/LiquidCalculator.cs
+++ Assets/Scripts/Cup/LiquidCalculator.cs
@@ -2,5 +2,12 @@
 
 public class LiquidCalculator : MonoBehaviour
 {
+    private Collider2D _collider;
+
+    private void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
     
 }
