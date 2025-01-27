dcd94b1ff26428374c0c99b8549dc45b1b4213de
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-26 17:13:09 -0800

    Scoring working

--- Assets/Scripts/UI/PatronsUIStuff/CustomerController.cs
+++ Assets/Scripts/UI/PatronsUIStuff/CustomerController.cs
@@ -10,40 +10,20 @@ namespace UI.PatronsUIStuff
 {
     public class CustomerController : MonoBehaviour
     {
-        [SerializeField] private float gameStartPauseTime = 3f;
 
         private static readonly int EnterBarIndex = Animator.StringToHash("enterBar");
         private static readonly int LeaveBarIndex = Animator.StringToHash("leaveBar");
         [SerializeField] private Animator animator;
 
 
-        [ListDrawerSettings(ShowFoldout = true, DraggableItems = true, ShowIndexLabels = true)] [SerializeField]
-        private List<Recipe> possibleInitialRecipes;
 
-        [SerializeField] private RecipeEvent recipeChosenEvent;
-
-        private Recipe _initialRecipe;
-
-        private void Start()
-        {
-            _initialRecipe = possibleInitialRecipes[Random.Range(0, possibleInitialRecipes.Count)];
-
-            StartCoroutine(GameStartPause());
-        }
-
-        private IEnumerator GameStartPause()
-        {
-            yield return new WaitForSeconds(gameStartPauseTime);
-            recipeChosenEvent?.Raise(_initialRecipe);
-            EnterBar();
-        }
         
-        private void EnterBar()
+        public void EnterBar()
         {
             animator.SetTrigger(EnterBarIndex);
         }
 
-        private void ExitBar()
+        public void ExitBar()
         {
             animator.SetTrigger(LeaveBarIndex);
         }
---
+++ Assets/Scripts/UI/PatronsUIStuff/CustomersOverallController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Events;
+using Scriptable_Objects;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace UI.PatronsUIStuff
+{
+    public class CustomersOverallController : MonoBehaviour
+    {
+        [SerializeField] private float waitBetweenCustomers = 4f;
+        [SerializeField] private float gameStartPauseTime = 3f;
+
+        [ListDrawerSettings(ShowFoldout = true, DraggableItems = true, ShowIndexLabels = true)] [SerializeField]
+        private List<Recipe> possibleInitialRecipes;
+
+        [ListDrawerSettings(ShowFoldout = true, DraggableItems = true, ShowIndexLabels = true)] [SerializeField]
+        private List<CustomerController> customerControllers;
+
+        [SerializeField] private RecipeEvent recipeChosenEvent;
+
+        private Recipe _currentRecipe;
+        private CustomerController _currentCustomer;
+
+        private void Start()
+        {
+            _currentRecipe = possibleInitialRecipes[Random.Range(0, possibleInitialRecipes.Count)];
+            StartCoroutine(GameStartPause());
+        }
+
+        private IEnumerator GameStartPause()
+        {
+            yield return new WaitForSeconds(gameStartPauseTime);
+            recipeChosenEvent?.Raise(_currentRecipe);
+            _currentCustomer = customerControllers[Random.Range(0, customerControllers.Count)];
+            _currentCustomer.EnterBar();
+        }
+
+        public void OnDrinkSubmitted()
+        {
+            _currentCustomer.ExitBar();
+
+            StartCoroutine(NewCustomerOrder());
+        }
+
+        private IEnumerator NewCustomerOrder()
+        {
+            yield return new WaitForSeconds(waitBetweenCustomers);
+            _currentCustomer = customerControllers[Random.Range(0, customerControllers.Count)];
+            _currentCustomer.EnterBar();
+            _currentRecipe = possibleInitialRecipes[Random.Range(0, possibleInitialRecipes.Count)];
+            recipeChosenEvent?.Raise(_currentRecipe);
+        }
+    }
+}
