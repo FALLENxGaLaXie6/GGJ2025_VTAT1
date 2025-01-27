7e94d40d26721c1c6adcddf398c917ece2db60bc
Joshua Steward <steward.joshua@rocketmail.com>
2025-01-26 18:27:37 -0800

    SFX for dudes working

---
+++ Assets/Scripts/Scriptable Objects/CustomerSFXData.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Scriptable_Objects
+{
+    [CreateAssetMenu(fileName = "Customer SFX Data", menuName = "Customer/Customer SFX Data", order = 0)]
+    public class CustomerSFXData : ScriptableObject
+    {
+        [field: SerializeField] public AudioClip GoodDrinkAudioClip { get; private set; }
+        [field: SerializeField] public AudioClip BadDrinkAudioClip { get; private set; }
+        [field: SerializeField] public AudioClip IntroAudioClip { get; private set; }
+
+
+        public void PlayGoodDrinkAudioClip(AudioSource audioSource)
+        {
+            audioSource.clip = GoodDrinkAudioClip;
+            audioSource.Play();
+        }
+
+        public void PlayBadDrinkAudioClip(AudioSource audioSource)
+        {
+            audioSource.clip = BadDrinkAudioClip;
+            audioSource.Play();
+        }
+
+        public void PlayIntroAudioClip(AudioSource audioSource)
+        {
+            audioSource.clip = IntroAudioClip;
+            audioSource.Play();
+        }
+    }
+}
--- Assets/Scripts/UI/PatronsUIStuff/CustomerController.cs
+++ Assets/Scripts/UI/PatronsUIStuff/CustomerController.cs
@@ -10,21 +10,41 @@ namespace UI.PatronsUIStuff
 {
     public class CustomerController : MonoBehaviour
     {
+        public bool IsActiveCustomer { get; private set; }
+
+
 
         private static readonly int EnterBarIndex = Animator.StringToHash("enterBar");
         private static readonly int LeaveBarIndex = Animator.StringToHash("leaveBar");
         [SerializeField] private Animator animator;
+        [field: SerializeField] public CustomerSFXData CustomerSfxData { get; private set; }
 
 
+        private void SetIsActiveCustomer(bool isActiveCustomer) => IsActiveCustomer = isActiveCustomer;
 
-        
+
         public void EnterBar()
         {
+            SetIsActiveCustomer(true);
             animator.SetTrigger(EnterBarIndex);
+            CustomerSfxData.PlayIntroAudioClip(GetComponent<AudioSource>());
+        }
+
+        public void OnGoodDrinkMade()
+        {
+            if (!IsActiveCustomer) return;
+            CustomerSfxData.PlayGoodDrinkAudioClip(GetComponent<AudioSource>());
+        }
+
+        public void OnBadDrinkMade()
+        {
+            if (!IsActiveCustomer) return;
+            CustomerSfxData.PlayBadDrinkAudioClip(GetComponent<AudioSource>());
         }
 
         public void ExitBar()
         {
+            SetIsActiveCustomer(false);
             animator.SetTrigger(LeaveBarIndex);
         }
     }
